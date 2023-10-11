d2e45cfc41df173158e1482b26d63047a96382f4
Mizuki-OHASHI <ohashi-mizuki0510@g.ecc.u-tokyo.ac.jp>
2023-10-11 22:05:26 +0900

    debug

--- brainStorming/app/usecase/posts.go
+++ brainStorming/app/usecase/posts.go
@@ -9,7 +9,7 @@ import (
 func GetPosts() ([]model.Post, error) {
 	posts, err := dao.GetPosts()
 	if err != nil {
-		return nil, errors.New("Failed to get posts")
+		return nil, errors.New("failed to get posts")
 	}
 	return posts, nil
 }
@@ -17,7 +17,7 @@ func GetPosts() ([]model.Post, error) {
 func GetPost(id string) (model.Post, error) {
 	post, err := dao.GetPost(id)
 	if err != nil {
-		return model.Post{}, errors.New("Failed to get post")
+		return model.Post{}, errors.New("failed to get post")
 	}
 	return post, nil
 }
@@ -25,7 +25,7 @@ func GetPost(id string) (model.Post, error) {
 func CreatePost(post *model.Post) error {
 	err := dao.CreatePost(post)
 	if err != nil {
-		return errors.New("Failed to create post")
+		return errors.New("failed to create post")
 	}
 	return nil
 }
@@ -33,7 +33,7 @@ func CreatePost(post *model.Post) error {
 func UpdatePost(post *model.Post) error {
 	err := dao.UpdatePost(post)
 	if err != nil {
-		return errors.New("Failed to update post")
+		return errors.New("failed to update post")
 	}
 	return nil
 }
@@ -41,7 +41,7 @@ func UpdatePost(post *model.Post) error {
 func DeletePost(id string) error {
 	err := dao.DeletePost(id)
 	if err != nil {
-		return errors.New("Failed to delete post")
+		return errors.New("failed to delete post")
 	}
 	return nil
 }
@@ -49,7 +49,7 @@ func DeletePost(id string) error {
 func GetPostsByUser(userId string) ([]model.Post, error) {
 	posts, err := dao.GetPostsByUser(userId)
 	if err != nil {
-		return nil, errors.New("Failed to get posts by user")
+		return nil, errors.New("failed to get posts by user")
 	}
 	return posts, nil
 }
--- brainStorming/app/usecase/users.go
+++ brainStorming/app/usecase/users.go
@@ -11,7 +11,7 @@ import (
 func GetUsers() ([]model.User, error) {
 	users, err := dao.GetUsers()
 	if err != nil {
-		return nil, errors.New("Failed to get users")
+		return nil, errors.New("failed to get users")
 	}
 	return users, nil
 }
@@ -19,7 +19,7 @@ func GetUsers() ([]model.User, error) {
 func GetUser(id string) (model.User, error) {
 	user, err := dao.GetUser(id)
 	if err != nil {
-		return model.User{}, errors.New("Failed to get user")
+		return model.User{}, errors.New("failed to get user")
 	}
 	return user, nil
 }
@@ -27,16 +27,16 @@ func GetUser(id string) (model.User, error) {
 func CreateUser(user *model.User) error {
 	exists, err := dao.IsUsernameExists(user.Name)
 	if err != nil {
-		return errors.New("Failed to check if username exists")
+		return errors.New("failed to check if username exists")
 	}
 	if exists {
-		return errors.New("Username already exists")
+		return errors.New("username already exists")
 	}
 
 	user.ID = uuid.New().String()
 	err = dao.CreateUser(user)
 	if err != nil {
-		return errors.New("Failed to create user")
+		return errors.New("failed to create user")
 	}
 	return nil
 }
@@ -44,7 +44,7 @@ func CreateUser(user *model.User) error {
 func UpdateUser(user *model.User) error {
 	err := dao.UpdateUser(user)
 	if err != nil {
-		return errors.New("Failed to update user")
+		return errors.New("failed to update user")
 	}
 
 	return nil
@@ -53,7 +53,7 @@ func UpdateUser(user *model.User) error {
 func DeleteUser(id string) error {
 	err := dao.DeleteUser(id)
 	if err != nil {
-		return errors.New("Failed to delete user")
+		return errors.New("failed to delete user")
 	}
 	return nil
 }
